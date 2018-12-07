00077e6b042954bd5d5bf526c1588aef2ad12bc0
mailmindlin <mailmindlin@users.noreply.github.com>
2018-12-07 18:42:51 -0500

    Add pipeline contexts

--- cpu.py
+++ cpu.py
@@ -1,7 +1,57 @@
-from enum import Enum
+from ir import MIPSInstruction, MIPSRegister, Node
+from typing import Dict, List, Optional, Tuple, Iterable
+from logger import LogEvent, PipelineExitEvent, PipelineStallEvent, StageAdvanceEvent, InstructionFetchEvent, ExId
 
-class TimingEvent(object):
-    pass
+
+class PipelineContext(object):
+    def __init__(self, exId: ExId, node: Node):
+        self.exId = exId
+        self.node = node
+
+
+IFContext = PipelineContext
+
+
+class IDContext(PipelineContext):
+    """
+    Fields:
+        rsValue
+            Value of rs register
+        rtValue
+            Value of rt register
+        rdTarget
+            Register to write to
+    """
+    rsValue: int
+    rtValue: int
+    rdTarget: MIPSRegister
+
+    def __init__(self, source: IFContext, rsValue: int, rtValue: int, rdTarget: MIPSRegister):
+        return super().__init__(source.exId, source.node)
+        self.rsValue = rsValue
+        self.rtValue = rtValue
+        self.rdTarget = rdTarget
+
+
+class EXContext(PipelineContext):
+    """
+    Fields:
+        rdValue
+            Computed output value
+        rdTarget
+            Register to write to
+    """
+    rdValue: int
+    rdTarget: MIPSRegister
+
+    def __init__(self, source: IDContext, rdValue: int, rdTarget: Optional[MIPSRegister] = None):
+        return super().__init__(source.exId, source.node)
+        self.rdValue = rdValue
+        self.rdTarget = rdTarget or source.rdTarget
+
+
+MEMContext = EXContext
+
 
 class CPU(object):
     """

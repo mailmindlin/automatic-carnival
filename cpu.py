4b4d01558898d2024b4587360b58383ac77e2542
levenj3 <35755500+levenj3@users.noreply.github.com>
2018-12-07 17:19:24 -0500

    ooof

--- cpu.py
+++ cpu.py
@@ -25,12 +25,23 @@ class CPU(object):
     def updateCycle(self):
         """PC=0"""
         while True:
-            if !pipeStages[FX]:
-                pipeline.append([instructions[i], [0 for number in xrange(PC)]])
+            #all occupied stages empty out at the beginning of a cycle
+            for stage in pipeStages:
+                stage = False
+
+            #fetch new instruction
+            pipeline.append([instructions[i], [0 for number in xrange(PC)]])
 
             for instruction in pipeline:
-                #if the instruction is incomplete, and the next pipeline stage for the instruction is free
-                if max(instruction[1]!=WB) && pipeStages[max(instruction[1])]: 
-                    instruction[1].append()
+                #if the instruction is incomplete
+                if max(instruction[1]!=WB):
+                    #if the next pipeline stage is free
+                    if !pipeStages[max(instruction[1])]:
+                        #put the instruction through the next pipeline stage
+                        newStage = max(instruction[1])+1
+                        instruction[1].append(newStage)
+                        pipeStages[max(newStage)] = True
+                        #if after doing so 
+                        if 
             PC+=1;
 

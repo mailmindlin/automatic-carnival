50543b6cfed451646d0fab588a03dc72312a5fc0
levenj3 <35755500+levenj3@users.noreply.github.com>
2018-12-07 17:09:54 -0500

    oof

--- cpu.py
+++ cpu.py
@@ -9,12 +9,28 @@ class CPU(object):
 
     Fields:
         registers: Map[string, number]
-        ...
+        pipeStages: List[Bool]
+        pipeline: List[
+                       List[Node, List[number]]
+                      ]
+        instructions: List[Node]
+        PC: number
     """
+
+
     def __init__(self, src):
         self.src = src
+
     
     def updateCycle(self):
-        # TODO: impl
-        pass
-        
+        """PC=0"""
+        while True:
+            if !pipeStages[FX]:
+                pipeline.append([instructions[i], [0 for number in xrange(PC)]])
+
+            for instruction in pipeline:
+                #if the instruction is incomplete, and the next pipeline stage for the instruction is free
+                if max(instruction[1]!=WB) && pipeStages[max(instruction[1])]: 
+                    instruction[1].append()
+            PC+=1;
+

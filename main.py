e8ef7c5f6685f18e1c173a2d7955399de89f6e34
mailmindlin <mailmindlin@users.noreply.github.com>
2018-12-07 14:56:17 -0500

    Start on CLI entry

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import sys
+from parser import Parser
 
-def main(srcFile):
+def main(forwarding, srcFile): # type: (bool, str) -> None
     with open(srcFile, 'r') as f:
-        # TODO: read src
-        pass
+        src = f.read()
     # Parse
+    parser = Parser(src)
     # Run the thing
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print('Usage: automatic-carnival [src]')
+    if len(sys.argv) < 3:
+        print('Usage: automatic-carnival [f/n] [src]')
         exit(-1)
 
-    main(sys.argv[1])
+    if sys.argv[1] not in ('F', 'N'):
+        print(f"Error: Forwarding mode must be either 'F' or 'N' (actual: '{sys.argv[1]}')")
+        exit(-1)
+    main(sys.argv[1] == 'F', sys.argv[2])
